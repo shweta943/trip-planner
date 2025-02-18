03a263d737c6ac3c73a07d7fb62f8de6e85cc96b
shweta943 <shwetaakulkarni943@gmail.com>
2025-02-18 20:49:12 +0530

    Initialize firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBzlhiue1zdU0OfFCeHM1jPnXngCt3C7m0",
+    authDomain: "vite-project-39e0f.firebaseapp.com",
+    projectId: "vite-project-39e0f",
+    // storageBucket: "vite-project-39e0f.firebasestorage.app",
+    // messagingSenderId: "709346662297",
+    appId: "1:709346662297:web:6c0a4426157136089fa508",
+    // measurementId: "G-6ZFT869LBL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
