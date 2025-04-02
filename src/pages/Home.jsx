0511b486545dbbb4b5e4406ebd41dc6326ba9046
shweta943 <shwetakulkarni943@gmail.com>
2025-04-02 10:48:31 +0530

    Add API integartions and redux 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,19 +3,19 @@ import SignUpModal from '../components/SignUpModal';
 import HeroSection from '../components/HeroSection';
 import CardsSlider from '../components/CardsSlider';
 // import useAuthStatus from "../hooks/useAuthStatus";
-import geminiAi from '../config/geminiAi';
+import geminiAi from '../config/GeminiAI/geminiAi';
 // import getImages from '../config/getImageUrl';
-import getCollectionData from '../config/getDestinations';
+// import getCollectionData from '../config/';
 
 const Home = () => {
     // const test = useAuthStatus();
     // console.log('test: ', test);
     // const data = geminiAi();
     // console.log('data: ', data);
-    const images = getImages();
+    // const images = getImages();
     // console.log('imagesInComponent: ', images);
-    const destinations = getCollectionData();
-    console.log('destinations: ', destinations);
+    // const destinations = getCollectionData();
+    // console.log('destinations: ', destinations);
     
 
     return (
